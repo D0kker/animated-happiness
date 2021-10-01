781b9052ff204982d8aa01f08eadea3d56de4eef
Rodolfo Salazar <rosalazar@bgeneral.com>
2021-10-01 17:27:34 -0500

    daily

--- main.go
+++ main.go
@@ -22,6 +22,8 @@ func main() {
 
 	http.HandleFunc("/test", testHandler)
 	http.HandleFunc("/getAllModules", getAllModules)
+	http.HandleFunc("/metricsDaily", getAllModules)
+	http.HandleFunc("/metricsMonthly", getAllModules)
 
 	log.Printf("Server starting on port %v\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
@@ -32,6 +34,54 @@ func testHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func getAllModules(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Params:", r.URL.Query())
+
+	paramProject := r.URL.Query().Get("project")
+	paramBranch := r.URL.Query().Get("branch")
+	if paramProject == "" {
+		fmt.Println("No proporciono el parametro PROJECT")
+	}
+	if paramBranch == "" {
+		fmt.Println("No proporciono el parametro BRANCH")
+	}
+
+	xx := sonarqube.GetAllModules(w, r, paramProject)
+	var ff sonarqube.MetricsComponentList
+	var bb sonarqube.MetricsComponentList
+
+	for _, element := range xx.Front.Projects {
+		ff.Components = append(ff.Components, sonarqube.GetMetrics(w, r, element.Key, paramBranch))
+	}
+
+	for _, element := range xx.Back.Projects {
+		bb.Components = append(bb.Components, sonarqube.GetMetrics(w, r, element.Key, paramBranch))
+	}
+
+	dat, err := ioutil.ReadFile("html/template.html")
+	check(err)
+
+	dataHtml := html.CreateHome(string(dat), xx)
+	dataHtml = html.CreateBackend(dataHtml, bb, xx)
+	dataHtml = html.CreateFrontend(dataHtml, ff, xx)
+
+	var dir = ""
+	if (paramProject == "bel")	{
+		dir = "bel-personal/metrics/"
+	}
+	if (paramProject == "belcom")	{
+		dir = "bel-comercial/metrics/"
+	}
+
+	t := time.Now()
+	concatenated := fmt.Sprint("/apachebel/", dir, t.Year(), "-", int(t.Month()), "/report.html")
+	er2 := ioutil.WriteFile(concatenated, []byte(dataHtml), 0644)
+
+	check(er2)
+
+	fmt.Fprint(w, "READY")
+}
+
+func metricsDaily(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Params:", r.URL.Query())
 
 	paramProject := r.URL.Query().Get("project")
