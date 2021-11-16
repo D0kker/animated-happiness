b12cc32baf7ac931980a1c4d2e213c4261b78571
Rodolfo Salazar <rosalazar@bgeneral.com>
2021-11-16 10:13:54 -0500

    filters

--- html/buildReport.go
+++ html/buildReport.go
@@ -44,7 +44,7 @@ func getData(xx sonarqube.MetricsComponentList, pp sonarqube.ProjectListModules)
 		var test string
 
 		for i := range element.Component.Measures {
-			if !strings.Contains(element.Component.Name, "api") {
+			if !strings.Contains(element.Component.Name, "api") || !strings.Contains(element.Component.Name, "plugin") || !strings.Contains(element.Component.Name, "http.status")  || !strings.Contains(element.Component.Name, "login.expire")  || !strings.Contains(element.Component.Name, "qr.service") {
 				if element.Component.Measures[i].Metric == "files" {
 					files = element.Component.Measures[i].Value
 					i, _ := strconv.Atoi(files)
@@ -82,6 +82,56 @@ func getData(xx sonarqube.MetricsComponentList, pp sonarqube.ProjectListModules)
 	}
 
 	sonar_measuresT := fmt.Sprintf("<th class='text-right'>%v</th>	<th class='text-right'>%v</th>	<th class='text-right'>%.2f</th>	<th class='text-right'>%v</th>	<th class='text-right'>%v</th>	<th class='text-right'>%v</th>", filesT, testT, coverageT/float64(len(xx.Components)), codeSmellsT, bugsT, vulnerabilitiesT)
+
+	return htmlDataStruct{
+		sonar_measures: sonar_measures,
+		sonar_measuresT: sonar_measuresT,
+	}
+}
+
+func getDataFF(xx sonarqube.MetricsComponentList, pp sonarqube.ProjectListModules) htmlDataStruct {
+
+	var sonar_measures string
+	var filesT int
+	var codeSmellsT int
+	var bugsT int
+	var vulnerabilitiesT int
+
+	for index, element := range xx.Components {
+		var files string
+		var codeSmells string
+		var bugs string
+		var vulnerabilities string
+
+		for i := range element.Component.Measures {
+			if !strings.Contains(element.Component.Name, "api") || !strings.Contains(element.Component.Name, "plugin") || !strings.Contains(element.Component.Name, "http.status")  || !strings.Contains(element.Component.Name, "login.expire")  || !strings.Contains(element.Component.Name, "qr.service") {
+				if element.Component.Measures[i].Metric == "files" {
+					files = element.Component.Measures[i].Value
+					i, _ := strconv.Atoi(files)
+					filesT += i
+				}
+				if element.Component.Measures[i].Metric == "code_smells" {
+					codeSmells = element.Component.Measures[i].Value
+					i, _ := strconv.Atoi(codeSmells)
+					codeSmellsT += i
+				}
+				if element.Component.Measures[i].Metric == "bugs" {
+					bugs = element.Component.Measures[i].Value
+					i, _ := strconv.Atoi(bugs)
+					bugsT += i
+				}
+				if element.Component.Measures[i].Metric == "vulnerabilities" {
+					vulnerabilities = element.Component.Measures[i].Value
+					i, _ := strconv.Atoi(vulnerabilities)
+					vulnerabilitiesT += i
+				}
+			}
+		}
+
+		sonar_measures += fmt.Sprintf("<tr>		<td class='text-left'>%v</td>		<td class='text-left'>%v</td>		<td class='text-right'>%v</td>		<td class='text-right'>%v</td>		<td class='text-right'>%v</td>		<td class='text-right'>%v</td>		</tr>", index+1, element.Component.Name, files, test, coverage, codeSmells, bugs, vulnerabilities)
+	}
+
+	sonar_measuresT := fmt.Sprintf("	<th class='text-right'>%v</th>	<th class='text-right'>%v</th>	<th class='text-right'>%v</th>	<th class='text-right'>%v</th>", filesT, codeSmellsT, bugsT, vulnerabilitiesT)
 
 	return htmlDataStruct{
 		sonar_measures: sonar_measures,
@@ -166,7 +216,7 @@ func CreateBackend(htmlData string, xx sonarqube.MetricsComponentList, pp sonarq
 	
 	var ObjBackFilter sonarqube.MetricsComponentList
 	for _, element := range xx.Components {
-		if !strings.Contains(element.Component.Key, "api") {
+		if !strings.Contains(element.Component.Key, "api") || !strings.Contains(element.Component.Key, "plugin") {
 			ObjBackFilter.Components = append(ObjBackFilter.Components, element)
 		}
 	}
@@ -182,12 +232,12 @@ func CreateBackend(htmlData string, xx sonarqube.MetricsComponentList, pp sonarq
 }
 
 func CreateFrontend(htmlData string, xx sonarqube.MetricsComponentList, pp sonarqube.ProjectListModules) string {
-	htmlDataStruct := getData(xx, pp)
+	htmlDataStruct := getDataFF(xx, pp)
 
 	htmlData = strings.ReplaceAll(htmlData, "{{sonar_measures_rows_FF}}", htmlDataStruct.sonar_measures)
 	htmlData = strings.ReplaceAll(htmlData, "{{sonar_measures_FF}}", htmlDataStruct.sonar_measuresT)
-	htmlData = strings.ReplaceAll(htmlData, "{{sonar_complexities_rows_FF}}", htmlDataStruct.sonar_complexities)
-	htmlData = strings.ReplaceAll(htmlData, "{{sonar_complexities_FF}}", htmlDataStruct.sonar_complexitiesT)
+	// htmlData = strings.ReplaceAll(htmlData, "{{sonar_complexities_rows_FF}}", htmlDataStruct.sonar_complexities)
+	// htmlData = strings.ReplaceAll(htmlData, "{{sonar_complexities_FF}}", htmlDataStruct.sonar_complexitiesT)
 
 	return htmlData
 }
